c5681331f1f49e03290bfe87d21b39abe6c48115
Kuhloud <Lucemi_cheung@hotmail.com>
2025-06-09 15:39:07 +0200

    added github pages

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "./components/Home.vue";
-import Auth from "./components/Auth.vue";
 import { getAuthToken, setAuthToken } from "@/utils/auth";
 import { createPinia } from "pinia";
 
