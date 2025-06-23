544f8199ec3f31e401d789f4a4887b0f12bfe3a8
Kuhloud <Lucemi_cheung@hotmail.com>
2025-06-23 14:44:19 +0200

    asas

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Login from "@/components/registration/Login.vue";
+import Signup from "@/components/registration/Signup.vue";
+import Transfer from "@/components/Transfer.vue";
+import MockATM from "@/components/MockATM.vue";
+
+import TransferHistory from "@/components/TransferHistory.vue";
+
+import User from "@/components/User.vue";
+import InactiveList from "@/components/InactiveCustomers.vue";
+import UserDetails from "@/components/UserDetails.vue";
+import Profile from "@/components/profile/Profile.vue";
+
+import EmployeeDashboard from "./components/EmployeeDashboard.vue";
+import IbanSearch from "@/components/IbanSearch.vue";
+import Home from "../components/Home.vue";
+import EmployeeTransferUser from './components/EmployeeTransferUser.vue';
+import EmployeeAllTransactionsList from './components/EmployeeAllTransactionsList.vue';
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: '/', // Critical for GitHub Pages
+  base: '/Kuhloud.github.io', // Critical for GitHub Pages
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
