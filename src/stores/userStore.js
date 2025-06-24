b8c9fb220f2e98dbfd3c23fbd314fa163770e089
Kuhloud <Lucemi_cheung@hotmail.com>
2025-06-25 00:50:36 +0200

    removed try catch, because it doesn't have to do with the database

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -66,32 +66,22 @@ export const userStore = defineStore('store', {
     },
     async autologin() {
       const token = getAuthToken()
-      try {
-        if (!this.checkValidToken(token)) {
-          this.logout();
-          return;
-        }
-        this.token = token;
-        this.user_id = localStorage.getItem('user_id');
-        this.role = localStorage.getItem('role');
-      } catch (error) {
-        console.error('Auto-login failed:');
+      if (!this.checkValidToken(token)) {
+        this.logout();
+        return;
       }
+      this.token = token;
+      this.user_id = localStorage.getItem('user_id');
+      this.role = localStorage.getItem('role');
     },
     checkValidToken(token) {
       if (!token) {
-        return this.handleInvalidToken()
+        return false;
       }
       const payload = JSON.parse(atob(token.split('.')[1]));
       const isExpired = payload.exp * 1000 < Date.now();
-      if (isExpired) {
-        return this.handleInvalidToken()
-      }
-      return true;
-    },
-    handleInvalidToken() {
-      this.logout();
-      return false;
+      return !isExpired;
+
     },
     validateInput(email) {
       if (email === '') {
